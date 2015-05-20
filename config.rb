34dc7b6324300199094e27dedc843f0e55b1a457
Yoshihiro Fujita <yoshihiro_fujita@hot-factory.jp>
2015-05-20 15:28:29 +0900

    初回UP

---
+++ config.rb
@@ -0,0 +1,19 @@
+# //必要なcompass pluginsがあれば記述//
+require 'compass/import-once/activate'
+
+# //プロジェクトパスを記述(document rootに設定すると楽チン)//
+#project_path    = "/"
+
+# //出力パスを記述//
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+# //アウトプット設定//
+# ex :expanded or :nested or :compact or :compressed
+output_style = :nested
+line_comments = false
+color_output = false
