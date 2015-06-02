0b53172604b1799c20eb0f703a4123f0b37eb05a
Yoshihiro Fujita <yoshihiro_fujita@hot-factory.jp>
2015-06-02 19:41:42 +0900

    まだまだ作業途中。ひとまずsassのコンパイルは通るようにした。

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Home | SiteName</title>
+    <!-- if Wordpress Delete start-->
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="">
+    <meta property="og:title" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" itemprop="image" content="">
+    <meta property="og:site_name" content="">
+    <meta property="fb:admins" content="1270819342">
+    <meta property="fb:app_id" content="461262417219059">
+    <!-- if Wordpress Delete end-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/common.css">
+    <!--[if lt IE 9]>
+    <script src="js/libs/css3-mediaqueries.js"></script>
+    <script src="js/libs/ie.js"></script>
+    <link rel="stylesheet" href="css/ie.css">
+    <![endif]-->
+    <script src="js/libs/jquery.js"></script>
+</head>
+<body>
+    <div id="page">
+        <header>
+            <h1 class="title"><a href="#">タイトル</a></h1>
+            <nav id="gNav">
+                <ul>
+                    <li>
+                        <a href="#">nav ul li a</a>
+                    </li>
+                    <li>
+                        <a href="#">nav ul li a</a>
+                    </li>
+                    <li>
+                        <a href="#">nav ul li a</a>
+                    </li>
+                </ul>
+            </nav>
+            <!-- /#gNav -->
+        </header>
+            <div id="contents">
+
+        <div class="row">
+            <div class="col-3"><p>Grid 3</p></div>
+            <div class="col-3"><p>Grid 3</p></div>
+            <div class="col-3"><p>Grid 3</p></div>
+            <div class="col-3"><p>Grid 3</p></div>
+        </div>
+
+            </div>
+            <!-- #contents -->
+        <footer>
+            <p class="copyright">
+                <small><script>myDate = new Date() ;myYear = myDate.getFullYear ();document.write(myYear);</script> &copy; SiteName</small>
+            </p>
+        </footer>
+    </div>
+    <!-- /#page -->
+    <script src="js/plugin.js"></script>
+    <script src="js/common.js"></script>
+</body>
+</html>
