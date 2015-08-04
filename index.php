f9be4b0f2c61c9f85a9b3c83d4a19184aeb88a9d
Yoshihiro Fujita <yoshihiro_fujita@hot-factory.jp>
2015-08-04 16:45:36 +0900

    コーディングガイドに合わせてスリムに

--- index.php
+++ index.php
@@ -1,87 +1,61 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Home | SiteName</title>
-    <!-- if Wordpress Delete start-->
-    <meta property="og:type" content="website">
-    <meta property="og:description" content="">
-    <meta property="og:title" content="">
-    <meta property="og:url" content="">
-    <meta property="og:image" itemprop="image" content="">
-    <meta property="og:site_name" content="">
-    <meta property="fb:admins" content="1270819342">
-    <meta property="fb:app_id" content="461262417219059">
-    <!-- if Wordpress Delete end-->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/common.css">
-    <!--[if lt IE 9]>
-    <script src="js/libs/css3-mediaqueries.js"></script>
-    <script src="js/libs/ie.js"></script>
-    <link rel="stylesheet" href="css/ie.css">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Home | SiteName</title>
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="">
+  <meta property="og:title" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" itemprop="image" content="">
+  <meta property="og:site_name" content="">
+  <meta property="fb:admins" content="##########">
+  <meta property="fb:app_id" content="##########">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="default-style" content="style.css">
+  <link rel="stylesheet" href="/common/css/common.css">
+  <!--[if lt IE 9]>
+    <script src="/common/js/libs/css3-mediaqueries.js"></script>
+    <script src="/common/js/libs/ie.js"></script>
+    <link rel="stylesheet" href="/common/css/ie.css">
     <![endif]-->
-    <script src="js/libs/jquery.js"></script>
+  <script src="js/libs/jquery.js"></script>
 </head>
+
 <body>
-    <div id="page">
-        <header>
-            <hgroup>
-                <h1 class="title"><a href="#">タイトル</a></h1>
-                <nav id="gNav">
-                    <ul>
-                        <li>
-                            <a href="#">nav ul li a</a>
-                        </li>
-                        <li>
-                            <a href="#">nav ul li a</a>
-                        </li>
-                        <li>
-                            <a href="#">nav ul li a</a>
-                        </li>
-                    </ul>
-                </nav>
-                <!-- /#gNav -->
-            </hgroup>
-        </header>
-            <div id="contents">
-
-                <div class="row1">
-                    <div class="col-md-1">可変5カラム1</div>
-                    <div class="col-md-1">可変5カラム2</div>
-                    <div class="col-md-1">可変5カラム3</div>
-                    <div class="col-md-1">可変5カラム4</div>
-                    <div class="col-md-1">可変5カラム5</div>
-                </div>
-
-                <div class="row2">
-                    <div class="col-md-1">可変5カラム1</div>
-                    <div class="col-md-1">可変5カラム2</div>
-                    <div class="col-md-1">可変5カラム3</div>
-                    <div class="col-md-1">可変5カラム4</div>
-                    <div class="col-md-1">可変5カラム5</div>
-                </div>
-
-                <div class="row1">
-                    <div class="col-md-1">可変5カラム1</div>
-                    <div class="col-md-1">可変5カラム2</div>
-                    <div class="col-md-1">可変5カラム3</div>
-                    <div class="col-md-1">可変5カラム4</div>
-                    <div class="col-md-1">可変5カラム5</div>
-                </div>
-
-            </div>
-            <!-- #contents -->
-        <footer>
-            <p class="copyright">
-                <small><script>myDate = new Date() ;myYear = myDate.getFullYear ();document.write(myYear);</script> &copy; SiteName</small>
-            </p>
-        </footer>
+  <div id="toppage">
+    <header>
+        <h1 class="title"><a href="#">タイトル</a></h1>
+        <nav id="gNav">
+          <ul>
+            <li>
+              <a href="#">nav ul li a</a>
+            </li>
+            <li>
+              <a href="#">nav ul li a</a>
+            </li>
+            <li>
+              <a href="#">nav ul li a</a>
+            </li>
+          </ul>
+        </nav>
+        <!-- /#gNav -->
+    </header>
+    <div id="contents">
+    コンテンツ
     </div>
-    <!-- /#page -->
-    <script src="js/plugin.js"></script>
-    <script src="js/common.js"></script>
+    <!-- #contents -->
+    <footer>
+      <p class="copyright">
+        <small><script>myDate = new Date() ;myYear = myDate.getFullYear ();document.write(myYear);</script> &copy; SiteName</small>
+      </p>
+    </footer>
+  </div>
+  <!-- /#page -->
+  <script src="js/plugin.js"></script>
+  <script src="js/common.js"></script>
 </body>
+
 </html>
