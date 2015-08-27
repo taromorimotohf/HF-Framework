a95f246bc8297db62bf279d9ee142289f08b956f
Yoshihiro Fujita <yoshihiro_fujita@hot-factory.jp>
2015-08-27 16:00:46 +0900

    gulp taskにpleeeaseを追加

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,38 +1,51 @@
-//npm install --save-dev gulp gulp-watch gulp-sass gulp-ruby-sass
+//npm install --save-dev gulp gulp-watch gulp-ruby-sass gulp-pleeease
+
 /**************************************************
  * modules laod
  *************************************************/
 var gulp       = require( 'gulp' );
 var watch      = require( 'gulp-watch' );
 var rubysass   = require( 'gulp-ruby-sass' );
+var pleeease   = require( 'gulp-pleeease' );
 
 /**************************************************
  * path
  *************************************************/
-var cssSrcPath        = './common/sass';
 var cssDestPath       = './common/css';
 var scssPath          = './common/sass';
 
 /**************************************************
  * tasks
  *************************************************/
-
 /**
- * rubysass
+ * gulp-ruby-sas
  */
-gulp.task( 'rubysass', function() {
-    return rubysass( cssSrcPath, {
-            style   : 'expanded',
-            loadPath: [
-                cssSrcPath
-            ]
-        } )
-        .pipe( gulp.dest( cssDestPath ) );
-} );
+gulp.task( 'sass', function() {
+  return rubysass( scssPath, {
+    style   : 'expanded',
+    loadPath: [
+      scssPath
+    ]
+  })
+    .pipe( gulp.dest( cssDestPath ) );
+});
 
 /**
+ * gulp-pleeease
+ */
+gulp.task('pleeease', function() {
+  return gulp.src( cssDestPath + '/*.css' )
+    .pipe(pleeease({
+    autoprefixer: ['last 5 versions'],
+    minifier: true,
+  }))
+    .pipe(gulp.dest( cssDestPath ));
+});
+
+/**************************************************
  * watch
- */
-gulp.task( 'watch', ['rubysass'], function() {
-    gulp.watch( scssPath + '/*.scss', ['rubysass'] );
-} );
+ *************************************************/
+gulp.task('watch', function() {
+  gulp.watch( scssPath + '/*.scss', ['sass'] );
+  return gulp.watch([ cssDestPath + '/*.css' ], ['pleeease']);
+});
