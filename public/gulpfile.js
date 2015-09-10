f1629b851bd31790731b240219189de756b6dd28
Yoshihiro Fujita <yoshihiro_fujita@hot-factory.jp>
2015-09-10 16:44:48 +0900

    gulpfile.jsにimageminとfrontnoteを追加

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,51 +1,93 @@
-//npm install --save-dev gulp gulp-watch gulp-ruby-sass gulp-pleeease
+//npm install --save-dev gulp gulp-watch gulp-ruby-sass gulp-pleeease gulp-imagemin imagemin-pngquant gulp-frontnote
 
 /**************************************************
  * modules laod
  *************************************************/
-var gulp       = require( 'gulp' );
-var watch      = require( 'gulp-watch' );
-var rubysass   = require( 'gulp-ruby-sass' );
-var pleeease   = require( 'gulp-pleeease' );
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var rubysass = require('gulp-ruby-sass');
+var pleeease = require('gulp-pleeease');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var frontNote = require("gulp-frontnote");
 
 /**************************************************
  * path
  *************************************************/
-var cssDestPath       = './common/css';
-var scssPath          = './common/sass';
+var cssDestPath = './common/css';
+var scssPath = './common/sass';
 
 /**************************************************
- * tasks
+ * main tasks
  *************************************************/
 /**
- * gulp-ruby-sas
+ * gulp-ruby-sass SCSSファイルのコンパイル
  */
-gulp.task( 'sass', function() {
-  return rubysass( scssPath, {
-    style   : 'expanded',
-    loadPath: [
+gulp.task('sass', function () {
+  return rubysass(scssPath, {
+      style: 'expanded',
+      loadPath: [
       scssPath
     ]
-  })
-    .pipe( gulp.dest( cssDestPath ) );
+    })
+    .pipe(gulp.dest(cssDestPath));
 });
 
 /**
- * gulp-pleeease
+ * gulp-pleeease CSSのベンダープレフィックス付加や圧縮など
  */
-gulp.task('pleeease', function() {
-  return gulp.src( cssDestPath + '/*.css' )
+gulp.task('pleeease', function () {
+  return gulp.src(cssDestPath + '/*.css')
     .pipe(pleeease({
-    autoprefixer: ['last 5 versions'],
-    minifier: true,
-  }))
-    .pipe(gulp.dest( cssDestPath ));
+      autoprefixer: ['last 5 versions'],
+      minifier: false,
+    }))
+    .pipe(gulp.dest(cssDestPath));
+});
+
+/**************************************************
+ * option tasks
+ *************************************************/
+/**
+ * imagemin 画像の圧縮
+ */
+var paths = {
+  srcDir: 'common/img',
+  dstDir: 'common/img_min'
+}
+
+gulp.task('img', function () {
+  var srcGlob = paths.srcDir + '/**/*.+(jpg|jpeg|png|gif|svg)';
+  var dstGlob = paths.dstDir;
+  var imageminOptions = {
+    optimizationLevel: 7
+  };
+
+  gulp.src(srcGlob)
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{
+        removeViewBox: false
+      }],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest(dstGlob));
+});
+
+/**
+ * frontNote スタイルガイドの作成
+ */
+gulp.task('guide', function() {
+  gulp.src('common/css/**/*.css')
+    .pipe(frontNote({
+    out: '_guide',
+  }));
 });
 
 /**************************************************
  * watch
  *************************************************/
-gulp.task('watch', function() {
-  gulp.watch( scssPath + '/*.scss', ['sass'] );
-  return gulp.watch([ cssDestPath + '/*.css' ], ['pleeease']);
+gulp.task('watch', function () {
+  gulp.watch(scssPath + '/*.scss', ['sass']);
+  return gulp.watch([cssDestPath + '/*.css'], ['pleeease']);
 });
